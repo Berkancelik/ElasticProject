73485728b6539a89cdd6e4cea809557bda7c92b5
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-05-20 13:13:33 +0300

    Add project files.

---
+++ ElasticProject/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElasticProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public HomeController()
+        {
+
+        }
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
